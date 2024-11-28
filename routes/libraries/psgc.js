e5f2aaf6274fa99f721d552362c93c6659faeca9
jerold <jeroldmirafuentes@gmail.com>
2024-11-28 15:23:23 +0800

    added middlewares to the psgc library endpoints route

--- routes/libraries/psgc.js
+++ routes/libraries/psgc.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import * as librariesController from '../../controllers/libraries/psgc.js';
+import * as middleware from '../../middleware/auth.js';
 
 const route = express.Router();
 
-route.get('/regions', librariesController.fetchRegions);
+route.get('/regions', middleware.protectByToken, librariesController.fetchRegions);
 
-route.get('/provinces', librariesController.fetchProvinces);
+route.get('/provinces', middleware.protectByToken, librariesController.fetchProvinces);
 
-route.get('/munCities', librariesController.fetchMunCities);
+route.get('/munCities', middleware.protectByToken, librariesController.fetchMunCities);
 
-route.get('/barangays', librariesController.fetchBarangays);
+route.get('/barangays', middleware.protectByToken, librariesController.fetchBarangays);
 
 export default route;
