c746b2ebb5c211d78e6142ccfc78d742f4ab4f47
jerold <jeroldmirafuentes@gmail.com>
2024-12-17 13:22:04 +0800

    added middleware function for psgc routes

--- src/routes/seeder/psgc.js
+++ src/routes/seeder/psgc.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import * as psgcController from '../../controllers/seeder/psgc.js';
+import * as middleware from '../../middleware/auth.js';
 
 const route = express.Router();
 
-route.post('/create', psgcController.seed);
+route.post('/create', middleware.protectedByAdminCredsOnly, psgcController.seed);
 
-route.get('/get', psgcController.retrieve);
+route.get('/get', middleware.bearerTokenAuthOrBasicAuth, psgcController.retrieve);
 
-route.delete('/drop', psgcController.drop);
+route.delete('/drop', middleware.protectedByAdminCredsOnly, psgcController.drop);
 
 export default route;
